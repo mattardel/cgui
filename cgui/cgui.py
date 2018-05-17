5001ed0458f0636d32a628e4f4665fc918cced67
Matthew Ardeleanu <mardelea@uci.edu>
2018-05-17 11:48:32 -0700

    Changing homepage to index.html, adding empty stylesheet

--- cgui/cgui.py
+++ cgui/cgui.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_index():
+    return render_template('index.html')
 
 
 '''
