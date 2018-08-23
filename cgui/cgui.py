0948bf2602082379fbfc8a2aa35e948ceadeecce
Laura Marsh <lau.lyn.marsh@gmail.com>
2018-08-23 01:35:41 -0700

    logic changes pt 1

--- cgui/cgui.py
+++ cgui/cgui.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from cgui.funds import Funds
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+funds = Funds()
 
 @app.route('/')
 def show_index():
@@ -25,60 +27,8 @@ def show_investor():
 
 @app.route('/portfolio')
 def show_portfolio():
-    funds_dict = {
-            'AMCPX':'AMCAP Fund',
-            'AMRMX':'American Mutual Fund® Class A',
-            'INPAX':'Conservative Growth and Income Portfolio',
-            'BFCAX':'Corporate Bond Fund',
-            'DWGAX':'Developing World Growth and Income Fund',
-            'EBNAX':'Emerging Markets Bond Fund ',
-            'GBLAX':'Global Balanced Fund Class A',
-            'PGGAX':'Global Growth Portfolio',
-            'GWPAX':'Growth Portfolio',
-            'GAIOX':'Growth and Income Portfolio',
-            'BLPAX':'Moderate Growth and Income Portfolio',
-            'MFAAX':'Mortgage Fund',
-            'ANBAX':'Strategic Bond Fund',
-            'AFAXX':'U.S. Government Money Market Fund',
-            'AANTX':'2060 Target Date Retirement Fund',
-            'AEPGX':'EuroPacific Growth Fund',
-            'ANEFX':'The New Economy Fund',
-            'ANWPX':'New Perspective Fund',
-            'AGTHX':'The Growth Fund of America',
-            'SMCWX':'SMALLCAP World Fund',
-            'NEWFX':'New World Fund',
-            'CWGIX':'Capital World Growth and Income Fund',
-            'ANCFX':'Fundamental Investors',
-            'AIVSX':'The Investment Company of America',
-            'AWSHX':'Washington Mutual Investors Fund',
-            'CAIBX':'Capital Income Builder',
-            'AMECX':'The Income Fund of America',
-            'ABALX':'American Balanced Fund',
-            'AHITX':'American High Income Trust',
-            'BFIAX':'American Funds Inflation Linked Bond Fund',
-            'ASBAX':'Short-Term Bond Fund of America',
-            'AIBAX':'Intermediate Bond Fund of America',
-            'AMUSX':'U.S. Government Securities Fund',
-            'ABNDX':'The Bond Fund of America',
-            'CWBFX':'Capital World Bond Fund',
-            'AMHIX':'American High-Income Municipal Bond Fund',
-            'ASTEX':'American Funds Short-Term Tax-Exempt Bond Fund',
-            'AFTEX':'American Funds Tax-Exempt Fund of America',
-            'LTEBX':'Limited Term Tax-Exempt Bond Fund of America',
-            'TAFTX':'The Tax-Exempt Fund of California',
-            'TRBCX':'T. Rowe Price Blue Chip Growth Fund',
-            'VPMCX':'Vanguard PrimeCap Fund',
-            'FBGRX':'Fidelity Blue Chip Growth Fund',
-            'TLIIX':'TIAA-CREF Enhanced Lg Cp Gr Idx',
-            'POGRX':'PrimeCap Odyssey Growth Fund',
-            'VPCCX':'Vanguard PrimeCap Core Fund',
-            'FKDNX':'Franklin DynaTech Fund',
-            'GLCGX':'Goldman Sachs Large Cap Gr Insghts Fd',
-            'BIAFX':'Brown Advisory Flexible Equity Fund',
-            'HACAX':'Harbor Capital Appreciation Fund'
-    }
     # route to investor page
-    return render_template('portfolio.html', mutual_funds=funds_dict)
+    return render_template('portfolio.html', funds_dict=funds.funds_dict, funds_closings=funds.closings_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
