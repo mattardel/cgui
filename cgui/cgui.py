ab40d63a3a1180924f70d17ff3ca85e8d252ac4b
fguan0529 <guan.fanny@gmail.com>
2018-08-27 01:51:40 -0700

    Funds screener merged onto main page

--- cgui/cgui.py
+++ cgui/cgui.py
@@ -1,12 +1,110 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, json, jsonify
+from forms.forms import ScreeningForm
+import random
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/')
+app.secret_key="capgroupunpaid"
+
+@app.route('/', methods=['GET', 'POST'])
 def show_index():
-    return render_template('index.html')
+    form = ScreeningForm()
+
+    # open json files
+    fund_data_json_url = 'fund_data.json'
+    form_data_json_url = 'form_data.json'
+    fund_data = json.load(open(fund_data_json_url))
+    form_data = json.load(open(form_data_json_url))
+
+    # store american funds data into arrays by fund type
+    growth = []
+    bond = []
+    ps = []
+    rips = []
+    tdf = []
+
+    for obj in fund_data['americanFunds']:
+        if obj['type'] == 'Growth':
+            growth.append(obj)
+        elif obj['type'] == 'Bond':
+            bond.append(obj)
+        elif obj['type'] == 'PS':
+            ps.append(obj)
+        elif obj['type'] == 'RIPS':
+            rips.append(obj)
+        elif obj['type'] == 'TDF':
+            tdf.append(obj)
+
+    american_funds = [growth, bond, ps, rips, tdf]
+    type = {'growth': 0, 'bond': 1, 'ps': 2, 'rips': 3, 'tdf': 4}
+    # END store american funds data into arrays by fund type
+
+    # list to store fund screener recommendations
+    recs = []
+    recs.append(' ')
+    recs.append(' ')
+
+    if request.method == 'POST':
+        # clear recs list
+        recs = []
+
+        # update form data json
+        with open(form_data_json_url, 'w') as f:
+            json.dump(request.form, f)
+        form_data = json.load(open(form_data_json_url))
+
+        # using random number for fund selection
+        # future iterations should use actual calculation and logic for fund selection
+
+        # IF retirement
+        if form_data['screen'] == 'retirement':
+            # pick from RIPS
+            randInt = random.randint(0, len(rips) - 1)
+            recs.append(rips[randInt])
+            print(recs[0])
+
+            # pick from TDF
+            randInt = random.randint(0, len(tdf) - 1)
+            recs.append(tdf[randInt])
+            print(recs[1])
+
+            print('retirement')
+
+        # IF wealth
+        elif form_data['screen'] == 'wealth':
+            # pick from growth
+            randInt = random.randint(0, len(growth) - 1)
+            recs.append(growth[randInt])
+            print(recs[0])
+
+            # pick from PS
+            randInt = random.randint(0, len(ps) - 1)
+            recs.append(ps[randInt])
+            print(recs[1])
+
+            print('wealth')
+
+        # IF goal
+        elif form_data['screen'] == 'goal':
+            # pick from bond
+            randInt = random.randint(0, len(bond) - 1)
+            recs.append(bond[randInt])
+            print(recs[0])
+
+            # pick from growth
+            randInt = random.randint(0, len(growth) - 1)
+            recs.append(growth[randInt])
+            print(recs[1])
+
+            print('goal')
+
+        return render_template('index.html', form=form, form_data=form_data, af=american_funds, type=type,
+                               recs=recs, submit=True)
+    elif request.method == 'GET':
+        return render_template('index.html', form=form, form_data=form_data, af=american_funds, type=type,
+                               recs=recs)
 
 @app.route('/test')
 def show_test():
@@ -28,18 +126,6 @@ def show_investor_details():
     #route to investor_details page
     return render_template('investor_details.html')
 
-@app.route('/advisor_search')
-def show_advisor_search():
-    #route to advisor search page
-    return render_template('advisor_search.html')
-
-@app.route('/investor_questions', methods=['GET', 'POST'])
-def screener():
-    form = ScreeningForm()
-    if request.method=='POST':
-        return render_template('investor_questions.html', form=form)
-    elif request.method=='GET':
-        return render_template('investor_questions.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ cgui/forms/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, SelectField, IntegerField, validators
+
+class ScreeningForm(Form):
+    name = StringField('Name')
+    screen = SelectField('Do you want to save for retirement, build wealth, or save for a specific goal?',
+                         choices=[('retirement', 'Retirement'), ('wealth', 'Build Wealth'),
+                                  ('goal', 'Save for a Goal')])
+    ageRet = IntegerField('What age do you plan to retire?')
+    currAge = IntegerField('What is your current age?')
+    nestegg = IntegerField('How much do you want saved for retirement?')
+    monthly = IntegerField('How much can you invest per month?')
+    esp = SelectField('Does your employer offer any stock or retirement plans?', choices=[('y', 'Yes'),('n', 'No')])
+
+    risk = SelectField('How aggressive do you want to be with your investments?', choices=[('cons', 'Conservative'), ('avg', 'Average'), ('agg', 'Aggressive')])
+    intl = SelectField('Are you interested in investing internationally?', choices=[('y', 'Yes'), ('n', 'No')])
+    sellSpeed = SelectField('Do you plan on selling early or minimizing taxes?', choices=[('early', 'Sell Early'), ('minTax', 'Minimize Taxes')])
+
+    goal = StringField('What goal are you saving for?')
+    eta = SelectField('When will you need the money?', [('soon', '1-2 years'), ('mid', '3-5 years'), ('far', '6-10 years'), ('long', '10+ years')])
+    goalNum = IntegerField('What is you savings goal?')
+    coll = SelectField('Are you saving for college?', choices=[('y', 'Yes'), ('n', 'No')])
+    kids = IntegerField('How many kids are you saving for?')
+    collType = SelectField('Are you planning on public or private college?', choices=[('pub', 'Public'), ('priv', 'Private')])
+    loan = SelectField('Are you saving for a down payment on a loan?', choices=[('y', 'Yes'), ('n', 'No')])
+    submit = SubmitField('send')
